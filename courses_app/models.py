972aec4efac770739cc58da5716f8c7fc5ceb2a0
Rene Montoya <rmontoya.we@gmail.com>
2020-12-06 21:44:58 -0800

    finishing assignment before attempting bonuses

--- courses_app/models.py
+++ courses_app/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class myManager(models.Manager):
+    def validator(self, postData):
+        errors = {}
+        if len(postData['name']) < 6:
+            errors['name'] = "Name must be more than 5 characters"
+        if len(postData['description']) < 16:
+            errors['description'] = "Description must be more than 15 characters"
+        return errors
+
 class theCourse(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
+    objects = myManager()
--- courses_app/views.py
+++ courses_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import theCourse
+from django.contrib import messages
+
 
 # Create your views here.
 def index(request):
@@ -9,6 +11,11 @@ def index(request):
     return render(request, "index.html", context)
 
 def add(request):
+    errors = theCourse.objects.validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
     theCourse.objects.create(
         name = request.POST['name'],
         description = request.POST['description']
